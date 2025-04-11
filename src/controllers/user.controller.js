e04f804b0917d68ea7386faf928a652c3754dbc2
Zuned khan <zunedkhan107@gmail.com>
2025-04-11 16:49:41 +0530

    add user account management features; implement change password, get current user, update account details, and user profile image functionalities

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -216,12 +216,198 @@ const logoutUser = asyncHandler( async(req, res) => {
        }
     })
 
+    const changeCurrentPassword = asyncHandler(async (req, res) => {
+        const {oldPassword, newPassword} = req.body
+         
+        const user = await User.findById(req.user?._id)
+        const isPasswordCorrect = await user.isPasswordValid(oldPassword)
+
+        if (!isPasswordCorrect) {
+            throw new ApiError(401, "Invalid old Password")
+        }
+        user.password = newPassword
+        await user.save({validateBeforeSave: false})
+
+        return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Password Chenged Succesfully"))
+    })
+
+    const getCurrentUser = asyncHandler(async(req, res) => {
+        return res
+        .status(200)
+        .json(200, req.user, "current User fatched successfully")
+    })
+
+    const updateAccountDetails = asyncHandler(async(req, res) => {
+        const {fullName, email} = req.body
+
+        if (!(fullName || email)) {
+            throw new ApiError(400, "FullName or email is required")
+            
+        }
+        const user = await User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                $set:{
+                    fullName,
+                    email
+                }
+            },
+            {new: true}
+        ).select("-password")
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Account details update Successfully")
+        )
+    })
+
+    const updateUserAvatar = asyncHandler(async(req, res) => {
+        const avatarLocalPath = req.file?._id
+
+        if (!avatarLocalPath) {
+            throw new ApiError(401, "Avatar is Required")
+        }
+
+        const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+        if (!avatar.url) {
+            throw new ApiError(401, "Avatar Path is Invaelid")
+        }
+
+       const user =  await User.findByIdAndUpdate(
+            req.file?._id,
+            {
+                $set: {
+                    avatar: avatar.url
+                }
+            },
+            {new: true}
+        ).select("-password")
+
+        return res
+        .status(200)
+        .json(200,
+             avatar, "Avatar updated Successfully"
+        )
+    })
+
+    const updateUserCoverImage = asyncHandler(async(req, res) => {
+        const coverImageLocalPath = req.file?._id
+
+        if (!coverImageLocalPath) {
+            throw new ApiError(401, "Avatar is Required")
+        }
+
+        const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+        if (!coverImage.url) {
+            throw new ApiError(401, "CoverImage Path is Invaelid")
+        }
+
+        const user = await User.findByIdAndUpdate(
+            req.file?._id,
+            {
+                $set: {
+                    coverImage: coverImage.url
+                }
+            },
+            {new: true}
+        ).select("-password")
+
+        return res
+        .status(200)
+        .json(200,
+             user, "Cover Image updated Successfully"
+        )
+    })
+
+    const getUserChannelProfile = asyncHandler(async(req, res) => {
+        const {username} = req.params;
+
+        if (!username) {
+            throw new ApiError(400, "username is not exits")
+        }
+
+        const channel = User.aggregate([
+            {
+                $match:{
+                    username : username?.toLowerCase()
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as: "subscribers"
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as: "subscribering"
+                }
+            },
+            {
+                $addFields: {
+                    subscriberCount: {
+                        $size: "$subscribers"
+                    },
+                    channelSubscriberCount: {
+                        $size: "$subscribering"
+                    },
+                    isSubscribered: {
+                        $cond: {
+                            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    fullName: 1,
+                    username: 1,
+                    email: 1,
+                    avatar: 1,
+                    coverImage: 1,
+                    subscriberCount: 1,
+                    channelSubscriberCount: 1,
+                    isSubscribered: 1
+                }
+            }
+            
+        ])
+        console.log(channel);
+        if (!channel) {
+            throw new ApiError(404, "channel does not exits")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, channel[0], "channel fached successfully")
+        )
+
+    })
 
 export {
          registerUser,
          loginUser,
          logoutUser,
-         refreshAccessToken
+         refreshAccessToken,
+         changeCurrentPassword,
+         getCurrentUser,
+         updateAccountDetails,
+         updateUserAvatar,
+         updateUserCoverImage,
+         getUserChannelProfile
        }
 
 
