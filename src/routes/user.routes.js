6d6c2e4686734a9c5889946e60361bba4a126db4
Zuned khan <zunedkhan107@gmail.com>
2025-04-12 15:08:42 +0530

    refactor user routes; add missing route handlers for password change, current user retrieval, account update, avatar and cover image updates, user channel profile, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from '../middlewares/multer.middleware.js'
 import { varifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,10 +20,17 @@ router.route("/register").post(
 
     router.route("/login").post(loginUser)
 
-
     // secured route
     router.route("/logout").post(varifyJWT, logoutUser)
-    router.route("/refresh-token").post(refreshAccessToken)
     
+    router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change_Password").post(varifyJWT, changeCurrentPassword)
+    router.route("/Current-User").get(varifyJWT, getCurrentUser)
+    router.route("/Account-Update").patch(varifyJWT, updateAccountDetails) // post all details ko update kar dega
+    router.route("/Update-Avatar").patch(varifyJWT, upload.single("avatar"), updateUserAvatar) // kyuki data req.file se aa rha hai
+    router.route("/Update-CoverImage").patch(varifyJWT, upload.single("coverImage"), updateUserCoverImage)
+    router.route("/c/:username").get(varifyJWT, getUserChannelProfile) // kyuki data params se aa rha hai and username hi likhna zruri hai
+    router.route("/history").get(varifyJWT, getWatchHistory)
+
 
 export default router
